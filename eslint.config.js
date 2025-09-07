3bc9f41af8d43ec7be2562db558009b754801bbb
Edward205204 <khoa123kd56cn@gmail.com>
2025-09-07 14:42:14 +0700

    start: setup project

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,30 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+// eslint.config.js
+import expo from 'eslint-config-expo/flat.js';
+import prettier from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
 
-module.exports = defineConfig([
-  expoConfig,
+export default defineConfig([
+  expo,
   {
     ignores: ['dist/*'],
-  },
+    plugins: {
+      prettier
+    },
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'always',
+          semi: true,
+          trailingComma: 'none',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true
+        }
+      ]
+    }
+  }
 ]);
